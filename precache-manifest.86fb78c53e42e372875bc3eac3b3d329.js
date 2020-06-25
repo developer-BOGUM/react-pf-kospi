e975b49c1dfa786d9dcd87e0aa18d70961054110
developer-BOGUM <bogumk1206@gmail.com>
2020-06-25 16:00:12 +0900

    Updates

---
+++ precache-manifest.86fb78c53e42e372875bc3eac3b3d329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d98008d291e934efef269370fb18fb",
+    "url": "/react-pf-kospi/index.html"
+  },
+  {
+    "revision": "f1f98e594e054a7823d0",
+    "url": "/react-pf-kospi/static/css/main.e0ecd59c.chunk.css"
+  },
+  {
+    "revision": "16e2f0bffd08dd966910",
+    "url": "/react-pf-kospi/static/js/2.367f7733.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-pf-kospi/static/js/2.367f7733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f98e594e054a7823d0",
+    "url": "/react-pf-kospi/static/js/main.eabd0a53.chunk.js"
+  },
+  {
+    "revision": "0f072d374123d6767710",
+    "url": "/react-pf-kospi/static/js/runtime-main.fb8d2b13.js"
+  }
+]);
